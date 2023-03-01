d172d4fe29ce1f8c52e6bc45453fd16ceee1f73c
lmp95 <lwinmgphyo95@gmail.com>
2023-03-01 20:27:19 +0630

    added canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import Canvas from './components/ScrollRuler';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const draw = (ctx, frameCount) => {
+        const canvasHeight = ctx.canvas.height;
+        for (let i = 0; i < 1000; i++) {
+            if (i % 2 === 0) {
+                ctx.moveTo(i * 20, canvasHeight / 2);
+                ctx.lineTo(i * 20, canvasHeight / 2 - 20);
+                ctx.stroke();
+            } else {
+                ctx.moveTo(i * 20, canvasHeight / 2);
+                ctx.lineTo(i * 20, canvasHeight / 2 - 50);
+                ctx.stroke();
+            }
+        }
+    };
 
-    return <div className='App'></div>;
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100vw', height: '100vh' }}>
+            <Canvas draw={draw} width='1000' height='500' />
+        </div>
+    );
 }
 
 export default App;
