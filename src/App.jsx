7cc838561d18eb47fd24bf02cecff7d56a1f4e70
lmp95 <lwinmgphyo95@gmail.com>
2023-03-09 20:34:34 +0630

    refactor drag slide move state

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,9 @@ import './App.css';
 import Canvas from './components/ScrollRuler';
 
 function App() {
-    const draw = (ctx, frameCount) => {
-        const canvasHeight = ctx.canvas.height;
-        for (let i = 0; i < 1000; i++) {
-            if (i % 2 === 0) {
-                ctx.moveTo(i * 20, canvasHeight / 2);
-                ctx.lineTo(i * 20, canvasHeight / 2 - 20);
-                ctx.stroke();
-            } else {
-                ctx.moveTo(i * 20, canvasHeight / 2);
-                ctx.lineTo(i * 20, canvasHeight / 2 - 50);
-                ctx.stroke();
-            }
-        }
-    };
-
     return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100vw', height: '100vh' }}>
-            <Canvas draw={draw} width='1000' height='500' />
+            <Canvas width='1000' height='500' />
         </div>
     );
 }
--- src/components/ScrollRuler/index.jsx
+++ src/components/ScrollRuler/index.jsx
@@ -1,28 +1,65 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
-const Canvas = (props) => {
-    const { draw, ...rest } = props;
+const Canvas = ({ start, end }, props) => {
     const canvasRef = useRef(null);
+    let touchPoints = [];
+    let coordinate = 0;
+    const [isDragging, setIsDragging] = useState(false);
+    const [translated, setTranslated] = useState(0);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        let frameCount = 0;
-        let animationFrameId;
+    const addTouchPoint = (shift) => touchPoints.push({ time: new Date().getTime(), shift });
 
-        const render = () => {
-            frameCount++;
-            draw(context, frameCount);
-            animationFrameId = window.requestAnimationFrame(render);
-        };
-        render();
+    const handleTouchStart = (event) => {
+        if (isDragging) return;
+        setIsDragging(true);
+        const { pageX, pageY } = event.touches?.[0] ?? event;
+        coordinate = pageX;
+    };
 
-        return () => {
-            window.cancelAnimationFrame(animationFrameId);
-        };
-    }, [draw]);
+    const handleTouchEnd = () => {
+        if (!isDragging) return;
+        setIsDragging(false);
+    };
 
-    return <canvas ref={canvasRef} {...rest} />;
+    const handleTouchMove = (event) => {
+        if (!isDragging) return;
+        const { pageX, pageY } = event.touches?.[0] ?? event;
+        addTouchPoint(pageX);
+        const delta = pageX - coordinate;
+        console.log(delta);
+        coordinate = pageX;
+    };
+
+    // const draw = (ctx, frameCount) => {
+    //     console.log(translated);
+    //     ctx.clearRect(translated, 0, 600, 400);
+    //     const canvasHeight = ctx.canvas.height;
+    //     ctx.moveTo(20, canvasHeight / 2);
+    //     ctx.lineTo(20, canvasHeight / 2 - 20);
+    //     ctx.stroke();
+    // for (let i = 0; i < 1000; i++) {
+    //     if (i % 2 === 0) {
+    //         ctx.moveTo(i * 20, canvasHeight / 2);
+    //         ctx.lineTo(i * 20, canvasHeight / 2 - 20);
+    //         ctx.stroke();
+    //     } else {
+    //         ctx.moveTo(i * 20, canvasHeight / 2);
+    //         ctx.lineTo(i * 20, canvasHeight / 2 - 50);
+    //         ctx.stroke();
+    //     }
+    // }
+    // };
+
+    return (
+        <canvas
+            {...props}
+            ref={canvasRef}
+            onMouseDown={(event) => handleTouchStart(event)}
+            onMouseMove={(event) => handleTouchMove(event)}
+            onMouseUp={handleTouchEnd}
+            onMouseLeave={handleTouchEnd}
+        />
+    );
 };
 
 export default Canvas;
