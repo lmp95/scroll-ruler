31f72f70ab0aac8d9a332ecee2242c22a10184dc
lmp95 <lwinmgphyo95@gmail.com>
2023-03-15 20:03:23 +0630

    fix canvas scroll

--- src/components/ScrollRuler/index.jsx
+++ src/components/ScrollRuler/index.jsx
@@ -2,10 +2,12 @@ import React, { useRef, useEffect, useState } from 'react';
 
 const Canvas = ({ start, end }, props) => {
     const canvasRef = useRef(null);
+    let translated = 0;
+    let moved = 0;
     let touchPoints = [];
     let coordinate = 0;
     const [isDragging, setIsDragging] = useState(false);
-    const [translated, setTranslated] = useState(0);
+    // const [translated, setTranslated] = useState(0);
 
     const addTouchPoint = (shift) => touchPoints.push({ time: new Date().getTime(), shift });
 
@@ -26,39 +28,54 @@ const Canvas = ({ start, end }, props) => {
         const { pageX, pageY } = event.touches?.[0] ?? event;
         addTouchPoint(pageX);
         const delta = pageX - coordinate;
-        console.log(delta);
         coordinate = pageX;
+        // setTranslated(delta);
     };
 
-    // const draw = (ctx, frameCount) => {
-    //     console.log(translated);
-    //     ctx.clearRect(translated, 0, 600, 400);
-    //     const canvasHeight = ctx.canvas.height;
-    //     ctx.moveTo(20, canvasHeight / 2);
-    //     ctx.lineTo(20, canvasHeight / 2 - 20);
-    //     ctx.stroke();
-    // for (let i = 0; i < 1000; i++) {
-    //     if (i % 2 === 0) {
-    //         ctx.moveTo(i * 20, canvasHeight / 2);
-    //         ctx.lineTo(i * 20, canvasHeight / 2 - 20);
-    //         ctx.stroke();
-    //     } else {
-    //         ctx.moveTo(i * 20, canvasHeight / 2);
-    //         ctx.lineTo(i * 20, canvasHeight / 2 - 50);
-    //         ctx.stroke();
-    //     }
-    // }
-    // };
+    useEffect(() => {
+        draw();
+    }, []);
+
+    const clear = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        translated += 20;
+        ctx.translate(-1, 0);
+        draw();
+    };
+
+    const draw = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, 10000, ctx.canvas.height);
+        const canvasHeight = ctx.canvas.height;
+        for (let i = 0; i < 50; i++) {
+            ctx.beginPath();
+            if (i % 2 === 0) {
+                ctx.moveTo(i * 20, canvasHeight / 2);
+                ctx.lineTo(i * 20, canvasHeight / 2 - 20);
+                ctx.stroke();
+            } else {
+                ctx.moveTo(i * 20, canvasHeight / 2);
+                ctx.lineTo(i * 20, canvasHeight / 2 - 50);
+                ctx.stroke();
+            }
+            ctx.closePath();
+        }
+    };
 
     return (
-        <canvas
-            {...props}
-            ref={canvasRef}
-            onMouseDown={(event) => handleTouchStart(event)}
-            onMouseMove={(event) => handleTouchMove(event)}
-            onMouseUp={handleTouchEnd}
-            onMouseLeave={handleTouchEnd}
-        />
+        <>
+            <canvas
+                {...props}
+                ref={canvasRef}
+                onMouseDown={(event) => handleTouchStart(event)}
+                onMouseMove={(event) => handleTouchMove(event)}
+                onMouseUp={handleTouchEnd}
+                onMouseLeave={handleTouchEnd}
+            />
+            <button onClick={() => clear()}>Clear</button>
+        </>
     );
 };
 
