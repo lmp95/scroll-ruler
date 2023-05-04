674a57ba84373764961eae56c186393692418019
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2023-05-04 20:40:56 +0630

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Canvas from './components/ScrollRuler';
 function App() {
     return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100vw', height: '100vh' }}>
-            <Canvas width='1000' height='500' />
+            <Canvas width='700' height='500' />
         </div>
     );
 }
--- src/components/ScrollRuler/index.jsx
+++ src/components/ScrollRuler/index.jsx
@@ -1,15 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-const Canvas = ({ start, end }, props) => {
+const Canvas = ({ width, height }, props) => {
     const canvasRef = useRef(null);
-    let translated = 0;
-    let moved = 0;
-    let touchPoints = [];
     let coordinate = 0;
     const [isDragging, setIsDragging] = useState(false);
-    // const [translated, setTranslated] = useState(0);
-
-    const addTouchPoint = (shift) => touchPoints.push({ time: new Date().getTime(), shift });
 
     const handleTouchStart = (event) => {
         if (isDragging) return;
@@ -26,32 +20,33 @@ const Canvas = ({ start, end }, props) => {
     const handleTouchMove = (event) => {
         if (!isDragging) return;
         const { pageX, pageY } = event.touches?.[0] ?? event;
-        addTouchPoint(pageX);
+        if (pageX === coordinate) return;
         const delta = pageX - coordinate;
         coordinate = pageX;
-        // setTranslated(delta);
+        redraw(delta);
     };
 
     useEffect(() => {
         draw();
     }, []);
 
-    const clear = () => {
+    const redraw = (delta) => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
-        translated += 20;
-        ctx.translate(-1, 0);
-        draw();
+        ctx.translate(delta > 0 ? 10 : -10, 0);
+        window.requestAnimationFrame(draw);
     };
 
     const draw = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, 10000, ctx.canvas.height);
+        ctx.clearRect(-100, 0, 10000, ctx.canvas.height);
         const canvasHeight = ctx.canvas.height;
-        for (let i = 0; i < 50; i++) {
+        ctx.font = '10px Arial';
+        for (let i = 1; i < 180; i++) {
             ctx.beginPath();
-            if (i % 2 === 0) {
+            ctx.fillText(i, i * 20 - 5, canvasHeight / 2 - 60);
+            if (i % 2 !== 0) {
                 ctx.moveTo(i * 20, canvasHeight / 2);
                 ctx.lineTo(i * 20, canvasHeight / 2 - 20);
                 ctx.stroke();
@@ -68,13 +63,14 @@ const Canvas = ({ start, end }, props) => {
         <>
             <canvas
                 {...props}
+                width={width}
+                height={height}
                 ref={canvasRef}
                 onMouseDown={(event) => handleTouchStart(event)}
                 onMouseMove={(event) => handleTouchMove(event)}
                 onMouseUp={handleTouchEnd}
                 onMouseLeave={handleTouchEnd}
             />
-            <button onClick={() => clear()}>Clear</button>
         </>
     );
 };
